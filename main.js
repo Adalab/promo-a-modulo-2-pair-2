cd64a4c4e510173151cc206476312662bc6f8475
reyesjerez <reyesjerezpe@gmail.com>
2023-12-16 11:01:36 +0100

    Add events

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const face = document.querySelector('.js_face');
+const head = document.querySelector('.js_head');
+
+head.addEventListener('click', handleClickHead );
+
+function handleClickHead (event) {
+    face.innerHTML = ';)'
+};
+
+head.addEventListener( 'mouseout', handleMouseoutHead );
+
+function handleMouseoutHead (event) {
+    face.innerHTML = ':)'
+};
